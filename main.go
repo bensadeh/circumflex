fcfe2fe03bc9839430a28bae0fdb58bfcd275177
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2020-08-15 08:44:08 +0200

    Add initial support for viewing comments

--- main.go
+++ main.go
@@ -1,47 +1,141 @@
 package main
 
 import (
-	"circumflex/client"
+	// "circumflex/client"
 	"circumflex/cmd"
-	"fmt"
+	"encoding/json"
+	"flag"
 
-	"github.com/gdamore/tcell"
-	"gitlab.com/tslocum/cview"
+	// "fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	// "github.com/gdamore/tcell"
+	"github.com/gocolly/colly"
+	// "gitlab.com/tslocum/cview"
 )
 
 func main() {
 	cmd.Execute()
 
-	client := client.NewHNClient()
-	pp, err := client.GetTopStories(30)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// for _, v := range *pp {
-	// 	fmt.Println(v.Title)
+	// client := client.NewHNClient()
+	// pp, err := client.GetTopStories(30)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
 	// }
 
-	app := cview.NewApplication()
-	list := cview.NewList()
+	// // for _, v := range *pp {
+	// // 	fmt.Println(v.Title)
+	// // }
 
-	list.SetBackgroundTransparent(false)
-	list.SetBackgroundColor(tcell.ColorDefault)
-	list.SetMainTextColor(tcell.ColorDefault)
-	list.SetSecondaryTextColor(tcell.ColorGray)
-	list.ShowSecondaryText(false)
+	// app := cview.NewApplication()
+	// list := cview.NewList()
 
-	reset := func() {
-		list.Clear()
-		for _, s := range *pp {
-			list.AddItem(s.Title, s.Author, 0, nil)
-		}
+	// list.SetBackgroundTransparent(false)
+	// list.SetBackgroundColor(tcell.ColorDefault)
+	// list.SetMainTextColor(tcell.ColorDefault)
+	// list.SetSecondaryTextColor(tcell.ColorGray)
+	// list.ShowSecondaryText(false)
+
+	// reset := func() {
+	// 	list.Clear()
+	// 	for _, s := range *pp {
+	// 		list.AddItem(s.Title, s.Author, 0, nil)
+	// 	}
+	// }
+
+	// reset()
+	// if err := app.SetRoot(list, true).EnableMouse(true).Run(); err != nil {
+	// 	panic(err)
+	// }
+
+	comments()
+}
+
+type comment struct {
+	Author  string `selector:"a.hnuser"`
+	URL     string `selector:".age a[href]" attr:"href"`
+	Comment string `selector:".comment"`
+	Replies []*comment
+	depth   int
+}
+
+func comments() {
+	var itemID string
+	flag.StringVar(&itemID, "id", "24089281", "hackernews post id")
+	flag.Parse()
+
+	if itemID == "" {
+		log.Println("Hackernews post id required")
+		os.Exit(1)
 	}
 
-	reset()
-	if err := app.SetRoot(list, true).EnableMouse(true).Run(); err != nil {
-		panic(err)
+	comments := make([]*comment, 0)
+
+	// Instantiate default collector
+	c := colly.NewCollector()
+
+	// Extract comment
+	c.OnHTML(".comment-tree tr.athing", func(e *colly.HTMLElement) {
+		width, err := strconv.Atoi(e.ChildAttr("td.ind img", "width"))
+		if err != nil {
+			return
+		}
+		// hackernews uses 40px spacers to indent comment replies,
+		// so we have to divide the width with it to get the depth
+		// of the comment
+		depth := width / 40
+		c := &comment{
+			Replies: make([]*comment, 0),
+			depth:   depth,
+		}
+		e.Unmarshal(c)
+		c.Comment = strings.TrimSpace(c.Comment[:len(c.Comment)-5])
+		if depth == 0 {
+			comments = append(comments, c)
+			return
+		}
+		parent := comments[len(comments)-1]
+		// append comment to its parent
+		for i := 0; i < depth-1; i++ {
+			parent = parent.Replies[len(parent.Replies)-1]
+		}
+		parent.Replies = append(parent.Replies, c)
+	})
+
+	c.Visit("https://news.ycombinator.com/item?id=" + itemID)
+
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+
+	// Dump json to the standard output
+	enc.Encode(comments)
+
+	// Pager logic
+	// pager := os.ExpandEnv("$PAGER")
+
+	// Could read $PAGER rather than hardcoding the path.
+	cmd := exec.Command("/usr/bin/less")
+
+	stringComments := ""
+	for _, s := range comments {
+		stringComments = stringComments + s.Comment
+	}
+
+	// Feed it with the string you want to display.
+	cmd.Stdin = strings.NewReader(stringComments)
+
+	// This is crucial - otherwise it will write to a null device.
+	cmd.Stdout = os.Stdout
+
+	// Fork off a process and wait for it to terminate.
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 }
