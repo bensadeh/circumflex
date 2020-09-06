89ca6c661d38265d0f2376fc2c54e4d06b372748
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2020-09-06 08:53:22 +0200

    Move and refactor

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"circumflex/cmd"
 	"fmt"
 
-	// "circumflex/client"
-
 	"log"
 	"os"
 	"os/exec"
@@ -15,11 +13,8 @@ import (
 	"strings"
 
 	"github.com/gdamore/tcell"
-	"github.com/gocolly/colly"
 	"gitlab.com/tslocum/cview"
 
-	// "github.com/rivo/tview"
-	"github.com/eidolon/wordwrap"
 	terminal "github.com/wayneashleyberry/terminal-dimensions"
 )
 
@@ -53,7 +48,8 @@ func main() {
 
 			for index, s := range *pp {
 				if index == i {
-					lessComments(s.ID)
+					commentTree := scrapeComments(s.ID)
+					outputStringToLess(commentTree)
 				}
 			}
 		})
@@ -76,96 +72,13 @@ func addListItems(list *cview.List, pp *[]feed.Item, app *cview.Application) {
 	}
 }
 
-type comment struct {
-	Author  string `selector:"a.hnuser"`
-	URL     string `selector:".age a[href]" attr:"href"`
-	Comment string `selector:".comment"`
-	Replies []*comment
-	depth   int
-}
-
-func lessComments(itemID string) {
-	comments := make([]*comment, 0)
-
-	// Instantiate default collector
-	c := colly.NewCollector()
-
-	// Extract comment
-	c.OnHTML(".comment-tree tr.athing", func(e *colly.HTMLElement) {
-		width, err := strconv.Atoi(e.ChildAttr("td.ind img", "width"))
-		if err != nil {
-			return
-		}
-		// hackernews uses 40px spacers to indent comment replies,
-		// so we have to divide the width with it to get the depth
-		// of the comment
-		depth := width / 40
-		c := &comment{
-			Replies: make([]*comment, 0),
-			depth:   depth,
-		}
-		e.Unmarshal(c)
-		c.Comment = strings.TrimSpace(c.Comment[:len(c.Comment)-5])
-		if depth == 0 {
-			comments = append(comments, c)
-			return
-		}
-		parent := comments[len(comments)-1]
-		// append comment to its parent
-		for i := 0; i < depth-1; i++ {
-			parent = parent.Replies[len(parent.Replies)-1]
-		}
-		parent.Replies = append(parent.Replies, c)
-	})
-
-	c.Visit("https://news.ycombinator.com/item?id=" + itemID)
-
-	commentTree := ""
-	for _, s := range comments {
-		commentTree = prettyPrintComments(*s, &commentTree, 0)
-
-	}
-
-	// Pager logic
-	// pager := os.ExpandEnv("$PAGER")
-
-	// Could read $PAGER rather than hardcoding the path.
+func outputStringToLess(output string) {
 	cmd := exec.Command("/usr/bin/less", "-R")
-
-	// Feed it with the string you want to display.
-	cmd.Stdin = strings.NewReader(commentTree)
-
-	// This is crucial - otherwise it will write to a null device.
+	cmd.Stdin = strings.NewReader(output)
 	cmd.Stdout = os.Stdout
 
-	// Fork off a process and wait for it to terminate.
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-}
-
-func prettyPrintComments(c comment, commentTree *string, indentlevel int) string {
-	x, _ := terminal.Width()
-	wrapper := wordwrap.Wrapper(int(x)-indentlevel-1, false)
-	wrapped := wrapper(c.Comment)
-	wrappedAndIndentedComment := wordwrap.Indent(wrapped, getIndentBlock(indentlevel), true)
-	wrappedAndIndentedAuthor := wordwrap.Indent(c.Author, getIndentBlock(indentlevel), true)
-
-	wrappedAndIndentedComment = "\033[1m" + wrappedAndIndentedAuthor + "\033[21m" + "\n" + wrappedAndIndentedComment + "\n" + "\n"
-
-	*commentTree = *commentTree + wrappedAndIndentedComment
-	for _, s := range c.Replies {
-		prettyPrintComments(*s, commentTree, indentlevel+5)
-	}
-	return *commentTree
-}
-
-func getIndentBlock(level int) string {
-	indentation := " "
-	for i := 1; i < level; i++ {
-		indentation = indentation + " "
-	}
-	return indentation
 }
