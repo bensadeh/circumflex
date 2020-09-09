3687b625720343dcf67cdec603d79285a660276d
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2020-09-09 17:48:51 +0200

    Add another field (last one, I swear)

--- main.go
+++ main.go
@@ -73,14 +73,15 @@ func main() {
 }
 
 type Comments struct {
-	Author  string      `json:"user"`
-	Title   string      `json:"title"`
-	Comment string      `json:"content"`
-	Time    string      `json:"time_ago"`
-	Points  int         `json:"points"`
-	URL     string      `json:"url"`
-	Domain  string      `json:"domain"`
-	Replies []*Comments `json:"comments"`
+	Author        string      `json:"user"`
+	Title         string      `json:"title"`
+	Comment       string      `json:"content"`
+	CommentsCount int         `json:"comments_count"`
+	Time          string      `json:"time_ago"`
+	Points        int         `json:"points"`
+	URL           string      `json:"url"`
+	Domain        string      `json:"domain"`
+	Replies       []*Comments `json:"comments"`
 }
 
 func addListItems(list *cview.List, pp *[]feed.Item, app *cview.Application) {
