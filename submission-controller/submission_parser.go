3aaed18e5da3b3fc136a0636c9ecca11eb8b70ba
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2020-10-10 17:46:48 +0200

    Do not export submission types

--- submission-controller/submission_parser.go
+++ submission-controller/submission_parser.go
@@ -22,9 +22,8 @@ const (
 	maximumStoriesToDisplay = 30
 )
 
-// SubmissionHandler stores submissions and pages
-type SubmissionHandler struct {
-	Submissions                 []Submission
+type submissionHandler struct {
+	Submissions                 []submission
 	MappedSubmissions           int
 	MappedPages                 int
 	StoriesListed               int
@@ -37,8 +36,8 @@ type SubmissionHandler struct {
 	MaxPages                    int
 }
 
-func NewSubmissionHandler() *SubmissionHandler {
-	sh := new(SubmissionHandler)
+func NewSubmissionHandler() *submissionHandler {
+	sh := new(submissionHandler)
 	sh.Application = cview.NewApplication()
 	sh.setShortcuts()
 	sh.Pages = cview.NewPages()
@@ -51,7 +50,7 @@ func NewSubmissionHandler() *SubmissionHandler {
 	return sh
 }
 
-func (sh *SubmissionHandler) setShortcuts() {
+func (sh *submissionHandler) setShortcuts() {
 	app := sh.Application
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyCtrlN {
@@ -77,7 +76,7 @@ func min(x, y int) int {
 	return x
 }
 
-func (sh *SubmissionHandler) nextPage() {
+func (sh *submissionHandler) nextPage() {
 	nextPage := sh.CurrentPage + 1
 
 	if nextPage > sh.MaxPages {
@@ -101,7 +100,7 @@ func (sh *SubmissionHandler) nextPage() {
 	sh.CurrentPage++
 }
 
-func (sh *SubmissionHandler) previousPage() {
+func (sh *submissionHandler) previousPage() {
 	previousPage := sh.CurrentPage - 1
 
 	if previousPage < 0 {
@@ -120,11 +119,11 @@ func (sh *SubmissionHandler) previousPage() {
 	l.SetCurrentItem(currentlySelectedItem)
 }
 
-func (sh *SubmissionHandler) getStoriesToDisplay() int {
+func (sh *submissionHandler) getStoriesToDisplay() int {
 	return sh.ViewableStoriesOnSinglePage
 }
 
-func setSelectedFunction(app *cview.Application, list *cview.List, sh *SubmissionHandler) {
+func setSelectedFunction(app *cview.Application, list *cview.List, sh *submissionHandler) {
 	list.SetSelectedFunc(func(i int, a string, b string, c rune) {
 		app.Suspend(func() {
 			for index := range sh.Submissions {
@@ -184,13 +183,11 @@ func outputStringToLess(output string) {
 	}
 }
 
-func (sh *SubmissionHandler) getSubmission(i int) Submission {
+func (sh *submissionHandler) getSubmission(i int) submission {
 	return sh.Submissions[i]
 }
 
-// Submission represents the JSON structure as
-// retrieved from cheeaun's unofficial HN API
-type Submission struct {
+type submission struct {
 	ID            int    `json:"id"`
 	Title         string `json:"title"`
 	Points        int    `json:"points"`
@@ -202,17 +199,17 @@ type Submission struct {
 	Type          string `json:"type"`
 }
 
-func (sh *SubmissionHandler) fetchSubmissions() {
+func (sh *submissionHandler) fetchSubmissions() {
 	sh.PageToFetchFromAPI++
 	p := strconv.Itoa(sh.PageToFetchFromAPI)
 	JSON, _ := http.Get("http://node-hnapi.herokuapp.com/news?page=" + p)
-	var submissions []Submission
+	var submissions []submission
 	_ = json.Unmarshal(JSON, &submissions)
 	sh.Submissions = append(sh.Submissions, submissions...)
 	sh.mapSubmissions()
 }
 
-func (sh *SubmissionHandler) mapSubmissions() {
+func (sh *submissionHandler) mapSubmissions() {
 	for sh.hasStoriesToMap() {
 		sub := sh.Submissions[sh.MappedSubmissions : sh.MappedSubmissions+sh.ViewableStoriesOnSinglePage]
 		list := createNewList(sh)
@@ -223,11 +220,11 @@ func (sh *SubmissionHandler) mapSubmissions() {
 	}
 }
 
-func (sh *SubmissionHandler) hasStoriesToMap() bool {
+func (sh *submissionHandler) hasStoriesToMap() bool {
 	return len(sh.Submissions)-sh.MappedSubmissions > sh.ViewableStoriesOnSinglePage
 }
 
-func createNewList(sh *SubmissionHandler) *cview.List {
+func createNewList(sh *submissionHandler) *cview.List {
 	list := cview.NewList()
 	list.SetBackgroundTransparent(false)
 	list.SetBackgroundColor(tcell.ColorDefault)
@@ -239,7 +236,7 @@ func createNewList(sh *SubmissionHandler) *cview.List {
 	return list
 }
 
-func addSubmissionsToList(list *cview.List, submissions []Submission, sh *SubmissionHandler) {
+func addSubmissionsToList(list *cview.List, submissions []submission, sh *submissionHandler) {
 	for _, submission := range submissions {
 		list.AddItem(
 			submission.getMainText(sh.MappedSubmissions),
@@ -251,17 +248,17 @@ func addSubmissionsToList(list *cview.List, submissions []Submission, sh *Submis
 	}
 }
 
-func (s Submission) getMainText(i int) string {
+func (s submission) getMainText(i int) string {
 	rank := i + 1
 	return strconv.Itoa(rank) + "." + getRankIndentBlock(rank) + s.Title + s.GetDomain()
 }
 
-func (s Submission) getSecondaryText() string {
+func (s submission) getSecondaryText() string {
 	return "[::d]" + "    " + s.getPoints() + " points by " + s.Author + " " +
 		s.Time + " | " + s.getComments() + " comments" + "[-:-:-]"
 }
 
-func (s Submission) GetDomain() string {
+func (s submission) GetDomain() string {
 	domain := s.Domain
 	if domain == "" {
 		return ""
@@ -269,11 +266,11 @@ func (s Submission) GetDomain() string {
 	return "[::d]" + " " + paren(domain) + "[-:-:-]"
 }
 
-func (s Submission) getComments() string {
+func (s submission) getComments() string {
 	return strconv.Itoa(s.CommentsCount)
 }
 
-func (s Submission) getPoints() string {
+func (s submission) getPoints() string {
 	return strconv.Itoa(s.Points)
 }
 
