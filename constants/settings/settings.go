837b66410f8a2158307d5c3eca86f9f05bfbe5e0
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-01-08 17:14:19 +0100

    Update settings descriptions

--- constants/settings/settings.go
+++ constants/settings/settings.go
@@ -6,13 +6,11 @@ const (
 	CommentWidthName          = "Comment Width"
 	CommentWidthKey           = "CLX_COMMENT_WIDTH"
 	CommentWidthDefault       = 70
-	CommentWidthDescription   = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
-		"incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
-		"laboris nisi ut aliquip ex ea commodo consequat."
-	IndentSizeName          = "Indent size"
-	IndentSizeKey           = "CLX_INDENT_SIZE"
-	IndentSizeDefault       = 4
-	IndentSizeDescription   = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
-		"incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
-		"laboris nisi ut aliquip ex ea commodo consequat."
+	CommentWidthDescription   = "Sets the maximum number of characters on each line for comments, " +
+		"replies, root submission comments and descriptions in settings. Set to 0 to use the whole screen."
+	IndentSizeName        = "Indent size"
+	IndentSizeKey         = "CLX_INDENT_SIZE"
+	IndentSizeDefault     = 4
+	IndentSizeDescription = "The number of whitespaces prepended to each reply, not included the " +
+		"color bar to the left of each reply."
 )
