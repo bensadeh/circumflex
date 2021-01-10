c0e52b63c5c3edfb4746c2ed27df5e1b7683e9f1
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-01-10 21:04:16 +0100

    Color numbers in description

--- constants/settings/settings.go
+++ constants/settings/settings.go
@@ -7,8 +7,8 @@ const (
 	CommentWidthName        = "Comment Width"
 	CommentWidthKey         = "CLX_COMMENT_WIDTH"
 	CommentWidthDefault     = 70
-	CommentWidthDescription = "Sets the maximum number of characters on each line for comments, " +
-		"replies, root submission comments and descriptions in settings. Set to 0 to use the whole screen."
+	CommentWidthDescription = "Sets the maximum number of characters on each line for comments, replies, root " +
+		"submission comments and descriptions in settings. Set to \u001B[31m0\u001B[0m to use the whole screen."
 	IndentSizeName        = "Indent Size"
 	IndentSizeKey         = "CLX_INDENT_SIZE"
 	IndentSizeDefault     = 4
@@ -17,12 +17,13 @@ const (
 	PreserveRightMarginName        = "Preserve Right Margin"
 	PreserveRightMarginKey         = "CLX_PRESERVE_RIGHT_MARGIN"
 	PreserveRightMarginDefault     = false
-	PreserveRightMarginDescription = "Shortens replies so that the total length, including indentation, is the same as " +
-		"the comment width. "
+	PreserveRightMarginDescription = "Shortens replies so that the total length, including indentation, is the same " +
+		"as the comment width. "
 	HighlightHeadlinesName        = "Highlight Headlines"
 	HighlightHeadlinesKey         = "CLX_HIGHLIGHT_HEADLINES"
 	HighlightHeadlinesDefault     = 1
 	HighlightHeadlinesDescription = "Highlights headlines containing the text \033[31mShow HN\033[0m, \033[35mAsk HN" +
-		"\033[0m, \033[34mTell HN\033[0m or \033[32mLaunch HN\033[0m. Can be set to 0 (No highlighting), 1 (inverse " +
-		"highlighting) and 2 (colored highlighting). Y Combinator Startup labels are colorized in both option 1 and 2."
+		"\033[0m, \033[34mTell HN\033[0m or \033[32mLaunch HN\033[0m. Can be set to \033[31m0\033[0m (No " +
+		"highlighting), \u001B[31m1\u001B[0m (inverse highlighting) and \u001B[31m2\u001B[0m (colored highlighting). " +
+		"Y Combinator Startup labels are colorized in both option \u001B[31m1\u001B[0m and \u001B[31m2\u001B[0m."
 )
