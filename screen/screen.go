d85611ffcf78ec8d7bb6a69bdd6d83ab6959305e
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-02-25 19:43:13 +0100

    Format

--- screen/screen.go
+++ screen/screen.go
@@ -6,11 +6,13 @@ import (
 
 func GetTerminalHeight() int {
 	height, _ := terminal.Height()
+
 	return int(height)
 }
 
 func GetTerminalWidth() int {
 	width, _ := terminal.Width()
+
 	return int(width)
 }
 
