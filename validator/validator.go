8a16b563f461a6f5f93d7546d699edbb53f59f5e
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-05-10 08:35:29 +0200

    Add another unsupported domain

--- validator/validator.go
+++ validator/validator.go
@@ -11,6 +11,7 @@ func IsValidDomain(title, domain, category string) (bool, string) {
 		strings.Contains(domain, "youtube") ||
 		strings.Contains(domain, "washingtonpost") ||
 		strings.Contains(domain, "sciencedirect") ||
+		strings.Contains(domain, "marketplace.atlassian.com") ||
 		strings.Contains(domain, "chrome.google.com") {
 		return false, "Reader Mode not supported on " + domain
 	}
