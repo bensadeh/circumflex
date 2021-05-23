a59d863c4e347be41a0b0098880d8fa17a942f54
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-05-23 20:50:50 +0200

    Add facebook to no-go list for Reader Mode

--- validator/validator.go
+++ validator/validator.go
@@ -33,6 +33,7 @@ func isInvalidDomain(domain string) bool {
 		"washingtonpost.com",
 		"sciencedirect.com",
 		"newsweek.com",
+		"facebook.com",
 		"atlasobscura.com",
 		"bloomberg.com",
 		"drive.google.com",
