1538c90c77173c30640c7dea595458031651d16d
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-05-27 21:55:47 +0200

    Remove unused functions

--- cli/cli.go
+++ cli/cli.go
@@ -6,10 +6,6 @@ import (
 	"strings"
 )
 
-const (
-	newLine = "\n"
-)
-
 func Less(input string) {
 	command := exec.Command("less", "-r")
 	command.Stdin = strings.NewReader(input)
@@ -18,55 +14,6 @@ func Less(input string) {
 	if err := command.Run(); err != nil {
 		panic(err)
 	}
-}
-
-func ParseWithLynx(input string) (string, string) {
-	references := getReferences(input)
-
-	numberOfReferences := len(strings.Split(references, newLine))
-	isManyReferences := numberOfReferences > 16
-
-	additionalArgument := ""
-
-	if isManyReferences {
-		references = ""
-		additionalArgument = "-nolist"
-	}
-
-	article := getArticle(input, additionalArgument)
-
-	return article, references
-}
-
-func getReferences(input string) string {
-	command := exec.Command("lynx", "-stdin", "-display_charset=utf-8", "-assume_charset=utf-8",
-		"-listonly", "-nomargins", "-nonumbers", "-hiddenlinks=ignore", "-notitle", "-dump")
-	command.Stdin = strings.NewReader(input)
-
-	references, err := command.Output()
-	if err != nil {
-		panic(err)
-	}
-
-	return string(references)
-}
-
-func getArticle(input string, additionalArgument string) string {
-	articleArguments := []string{
-		"-stdin", "-display_charset=utf-8", "-assume_charset=utf-8", "-dump",
-		"-hiddenlinks=ignore",
-	}
-
-	articleArguments = append(articleArguments, additionalArgument)
-	command := exec.Command("lynx", articleArguments...)
-	command.Stdin = strings.NewReader(input)
-
-	article, err := command.Output()
-	if err != nil {
-		panic(err)
-	}
-
-	return string(article)
 }
 
 func ClearScreen() {
