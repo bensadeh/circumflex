ea0edfb70cb142cb1ee67860dc292a15a01aa6fb
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-05-27 08:15:22 +0200

    Update messsages

--- constants/messages/messages.go
+++ constants/messages/messages.go
@@ -8,19 +8,15 @@ import (
 const (
 	separator = "-"
 
-	Refreshed          = "Refreshed"
-	Cancelled          = "Cancelled"
-	OfflineMessage     = "Could not fetch stories          [-]Press 'r' to retry or 'q' to quit"
-	ConfigConfirmation = "[::b]config.env[::-] will be created in [::r]~/.config/circumflex[::-], " +
-		"press Y to Confirm"
-	ConfigCreatedAt             = "Config created at [::b]~/.config./circumflex/config.env"
-	ConfigNotCreated            = "Could not create config file"
+	Refreshed                   = "Refreshed"
+	Cancelled                   = "Cancelled"
+	OfflineMessage              = "Could not fetch stories[-::] | [-]Press [::b]r[::-] to retry or [::b]q[::-] to quit"
 	CommentsNotFetched          = "Could not fetch comments"
 	EnterCommentSectionToUpdate = "[Enter comment section to update story]"
-	DeleteFromFavorites         = "[red]Delete[-] from Favorites? Press [::b]Y[::-] to Confirm"
-	AddToFavorites              = "[green]Add[-] to Favorites? Press [::b]Y[::-] to Confirm"
+	DeleteFromFavorites         = "[red]Delete[-] from Favorites? Press [::b]y[::-] to Confirm"
+	AddToFavorites              = "[green]Add[-] to Favorites? Press [::b]y[::-] to Confirm"
 	LessScreenInfo              = "You are now in 'less' • Press 'q' to return and 'h' for help"
-	HowToExitF                  = "[::d]Leave ID blank to return to main screen"
+	HowToExitF                  = "[::d]Leave ID blank to return to main screen[::-]"
 )
 
 func GetCircumflexStatusMessage() string {
