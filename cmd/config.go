be9c08afa78a08fbde60457516b9b898f94174ab
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-06-30 15:30:02 +0200

    Rename files

--- cmd/config.go
+++ cmd/config.go
@@ -1,22 +1,30 @@
 package cmd
 
 import (
-	"clx/constants/clx"
+	"clx/file"
+	"clx/settings"
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	rootCmd.AddCommand(configCmd)
+	rootCmd.AddCommand(versionCmd)
 }
 
-var configCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number of circumflex",
-	Long:  `Print the version number of circumflex`,
+var versionCmd = &cobra.Command{
+	Use:   "create_example_config",
+	Short: "Create a example config file",
+	Long:  `Create a example config file in ~/.config/circumflex/config.env`,
 	Run: func(cmd *cobra.Command, args []string) {
-		println("circumflex " + clx.Version)
+		err := file.WriteToFile(file.PathToConfigFile(), settings.GetConfigFileContents())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Example config file written to ~/.config/circumflex/config.env")
 		os.Exit(0)
 	},
 }
