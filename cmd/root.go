2773e4808118fb763fd9826b146583415c89aeb3
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-07-24 12:44:00 +0200

    Add comment and indent flag

--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,8 @@ var (
 	disableHistory bool
 	altIndentBlock bool
 	smileyEmojis   bool
+	commentWidth   int
+	indentSize     int
 )
 
 var rootCmd = &cobra.Command{
@@ -51,14 +53,22 @@ func Execute() error {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	rootCmd.PersistentFlags().BoolVarP(&plainHeadlines, "plain-headlines", "l", false,
 		"disable syntax highlighting for headlines")
-	rootCmd.PersistentFlags().BoolVarP(&plainComments, "plain-comments", "c", false,
+	rootCmd.PersistentFlags().BoolVarP(&plainComments, "plain-comments", "o", false,
 		"disable syntax highlighting for comments")
 	rootCmd.PersistentFlags().BoolVarP(&disableHistory, "disable-history", "d", false,
 		"disable marking stories as read")
-	rootCmd.PersistentFlags().BoolVarP(&altIndentBlock, "use-alt-indent-block", "i", false,
+	rootCmd.PersistentFlags().BoolVarP(&altIndentBlock, "use-alt-indent-block", "a", false,
 		"use alternate indentation block")
 	rootCmd.PersistentFlags().BoolVarP(&smileyEmojis, "smiley-emojis", "s", false,
 		"convert smileys to emojis")
+
+	rootCmd.PersistentFlags().IntVarP(&commentWidth, "comment-width", "c", settings.CommentWidthDefault,
+		"set the comment width")
+	_ = viper.BindPFlag(settings.CommentWidthKey, rootCmd.PersistentFlags().Lookup("comment-width"))
+
+	rootCmd.PersistentFlags().IntVarP(&indentSize, "indent-size", "i", settings.IndentSizeDefault,
+		"set the indent size")
+	_ = viper.BindPFlag(settings.IndentSizeKey, rootCmd.PersistentFlags().Lookup("indent-size"))
 
 	return rootCmd.Execute()
 }
