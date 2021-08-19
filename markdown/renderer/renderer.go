9f2b5f61c868ad380da636f7e223786cbd07a10d
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-08-19 21:24:40 +0200

    Add formatting for images

--- markdown/renderer/renderer.go
+++ markdown/renderer/renderer.go
@@ -1,6 +1,12 @@
 package renderer
 
-import "clx/markdown"
+import (
+	"clx/markdown"
+	"regexp"
+	"strings"
+
+	. "github.com/logrusorgru/aurora/v3"
+)
 
 func ToString(blocks []*markdown.Block) string {
 	output := ""
@@ -9,6 +15,9 @@ func ToString(blocks []*markdown.Block) string {
 		switch {
 		case block.Kind == markdown.Text:
 			output += renderText(block.Text) + "\n\n"
+
+		case block.Kind == markdown.Image:
+			output += renderImage(block.Text) + "\n\n"
 
 		default:
 			output += renderText(block.Text) + "\n\n"
@@ -20,4 +29,29 @@ func ToString(blocks []*markdown.Block) string {
 
 func renderText(text string) string {
 	return text
+}
+
+func renderImage(text string) string {
+	exp := regexp.MustCompile(`!\[(.*?)\]\(.*?\)`)
+	imageLabel := Magenta("Image: %%%").Faint().String()
+
+	text = exp.ReplaceAllString(text, imageLabel+Italic(`$1`).String()+"###")
+
+	lines := strings.Split(text, imageLabel)
+	output := ""
+
+	for i, line := range lines {
+		if i == 0 || line == "" {
+			continue
+		}
+
+		output += imageLabel + line + "\n\n"
+	}
+
+	// Remove ': ' if there is no image caption
+	output = strings.ReplaceAll(output, ": %%%\u001B[0m\u001B[3m\u001B[0m###", "\u001B[0m ")
+
+	output = strings.TrimSuffix(output, "\n\n")
+
+	return output
 }
