2850cfb103d8491a780e7d00a0de881fc10701b1
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-11-21 08:50:08 +0100

    Remove unused code

--- reader/reader.go
+++ reader/reader.go
@@ -1,14 +1,12 @@
 package reader
 
 import (
-	"clx/article"
 	"clx/markdown"
 	"clx/markdown/preprocessor"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
-	"os/exec"
 	"strings"
 	"time"
 
@@ -20,22 +18,6 @@ import (
 
 	"github.com/go-shiori/go-readability"
 )
-
-const (
-	newLine = "\n"
-)
-
-func Get(url string) (string, error) {
-	art, httpErr := fetch(url)
-	if httpErr != nil {
-		return "", fmt.Errorf("could not fetch url: %w", httpErr)
-	}
-
-	content, references := parseWithLynx(art.Content)
-	parsedArticle := article.Parse(art.Title, url, content, references)
-
-	return parsedArticle, nil
-}
 
 func GetNew(url string) (string, error) {
 	art, httpErr := fetch(url)
@@ -125,53 +107,4 @@ func fetch(rawURL string) (readability.Article, error) {
 	}
 
 	return art, nil
-}
-
-func parseWithLynx(input string) (string, string) {
-	references := getReferences(input)
-
-	numberOfReferences := len(strings.Split(references, newLine))
-	isManyReferences := numberOfReferences > 16
-
-	additionalArgument := ""
-
-	if isManyReferences {
-		references = ""
-		additionalArgument = "-nolist"
-	}
-
-	content := getContent(input, additionalArgument)
-
-	return content, references
-}
-
-func getReferences(input string) string {
-	command := exec.Command("lynx", "-stdin", "-display_charset=utf-8", "-assume_charset=utf-8",
-		"-listonly", "-nomargins", "-nonumbers", "-hiddenlinks=ignore", "-notitle", "-dump")
-	command.Stdin = strings.NewReader(input)
-
-	references, err := command.Output()
-	if err != nil {
-		panic(err)
-	}
-
-	return string(references)
-}
-
-func getContent(input string, additionalArgument string) string {
-	articleArguments := []string{
-		"-stdin", "-display_charset=utf-8", "-assume_charset=utf-8", "-dump",
-		"-hiddenlinks=ignore",
-	}
-
-	articleArguments = append(articleArguments, additionalArgument)
-	command := exec.Command("lynx", articleArguments...)
-	command.Stdin = strings.NewReader(input)
-
-	content, err := command.Output()
-	if err != nil {
-		panic(err)
-	}
-
-	return string(content)
 }
