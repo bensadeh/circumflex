73db62bed181f964daba455129e70f221ac8f67e
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-01-09 19:43:38 +0100

    Add man pages

--- cmd/clear.go
+++ cmd/clear.go
@@ -8,10 +8,9 @@ import (
 
 func clearCmd() *cobra.Command {
 	return &cobra.Command{
-		Use:   "clear",
-		Short: "Clear the history of visited IDs",
-		Long: "Clear the history of visited IDs from ~/.cache/circumflex/history.json.\n" +
-			"History is only cached if CLX_MARK_AS_READ is set to true.",
+		Use:                   "clear",
+		Short:                 "Clear the history of visited IDs",
+		Long:                  "Clear the history of visited IDs from ~/.cache/circumflex/history.json.",
 		Args:                  cobra.NoArgs,
 		DisableFlagsInUseLine: true,
 		Run: func(cmd *cobra.Command, args []string) {
---
+++ share/asciidoctor/convert_to_man.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+asciidoctor -b manpage clx.adoc --destination=../man/
