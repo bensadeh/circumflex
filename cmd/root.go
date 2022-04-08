2fdef38dae2d337380bfc4172eeeb1159cffd432
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-04-08 18:07:47 +0200

    Reroute clx to bubble tea list screen

--- cmd/root.go
+++ cmd/root.go
@@ -1,24 +1,9 @@
 package cmd
 
 import (
-	"clx/clx"
+	"clx/bubble"
 	clx2 "clx/constants/clx"
-	"clx/core"
-	"clx/indent"
-
 	"github.com/spf13/cobra"
-)
-
-var (
-	plainHeadlines       bool
-	commentWidth         int
-	plainComments        bool
-	disableHistory       bool
-	disableEmojis        bool
-	useRelativeNumbering bool
-	hideIndentSymbol     bool
-	debugMode            bool
-	headerType           int
 )
 
 func Root() *cobra.Command {
@@ -28,80 +13,16 @@ func Root() *cobra.Command {
 		Long:    "circumflex is a command line tool for browsing Hacker News in your terminal",
 		Version: clx2.Version,
 		Run: func(cmd *cobra.Command, args []string) {
-			config := getConfig()
-			config.IndentationSymbol = indent.GetIndentSymbol(hideIndentSymbol)
-
-			clx.Run(config)
+			bubble.Run()
 		},
 	}
 
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 
-	configureFlags(rootCmd)
-
 	rootCmd.AddCommand(addCmd())
 	rootCmd.AddCommand(clearCmd())
 	rootCmd.AddCommand(viewCmd())
+	rootCmd.AddCommand(legacyCmd())
 
 	return rootCmd
-}
-
-func configureFlags(rootCmd *cobra.Command) {
-	rootCmd.PersistentFlags().BoolVarP(&plainHeadlines, "plain-headlines", "p", false,
-		"disable syntax highlighting for headlines")
-	rootCmd.PersistentFlags().BoolVarP(&plainComments, "plain-comments", "o", false,
-		"disable syntax highlighting for comments")
-	rootCmd.PersistentFlags().BoolVarP(&disableHistory, "disable-history", "d", false,
-		"disable marking stories as read")
-	rootCmd.PersistentFlags().BoolVarP(&disableEmojis, "disable-emojis", "s", false,
-		"disable conversion of smileys to emojis")
-	rootCmd.PersistentFlags().BoolVarP(&useRelativeNumbering, "relative-numbering", "r", false,
-		"use relative numbering for submissions")
-	rootCmd.PersistentFlags().BoolVarP(&hideIndentSymbol, "hide-indent", "t", false,
-		"hide the indentation bar to the left of the reply")
-	rootCmd.PersistentFlags().IntVarP(&commentWidth, "comment-width", "c", core.GetConfigWithDefaults().CommentWidth,
-		"set the comment width")
-	rootCmd.PersistentFlags().IntVarP(&headerType, "header-type", "e", 0,
-		"set the header type on the main screen")
-	rootCmd.PersistentFlags().BoolVarP(&debugMode, "debug-mode", "q", false,
-		"mock endpoints")
-
-	rootCmd.Flag("debug-mode").Hidden = true
-}
-
-func getConfig() *core.Config {
-	config := core.GetConfigWithDefaults()
-
-	config.CommentWidth = commentWidth
-	config.HeaderType = headerType
-
-	if plainHeadlines {
-		config.HighlightHeadlines = false
-	}
-
-	if plainComments {
-		config.HighlightComments = false
-	}
-
-	if disableHistory {
-		config.MarkAsRead = false
-	}
-
-	if disableEmojis {
-		config.EmojiSmileys = false
-	}
-
-	if useRelativeNumbering {
-		config.RelativeNumbering = true
-	}
-
-	if hideIndentSymbol {
-		config.HideIndentSymbol = true
-	}
-
-	if debugMode {
-		config.DebugMode = true
-	}
-
-	return config
 }
--- go.mod
+++ go.mod
@@ -8,9 +8,10 @@ require (
 	github.com/MichaelMure/go-term-text v0.2.9
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/bobesa/go-domain-util v0.0.0-20190911083921-4033b5f7dd89
+	github.com/charmbracelet/bubbles v0.10.3
+	github.com/charmbracelet/bubbletea v0.20.1-0.20220407003955-f5ff931834c7
 	github.com/charmbracelet/glamour v0.3.0
 	github.com/charmbracelet/lipgloss v0.4.0
-	github.com/emirpasic/gods v1.12.0
 	github.com/gdamore/tcell/v2 v2.4.1-0.20210828201608-73703f7ed490
 	github.com/go-resty/resty/v2 v2.6.0
 	github.com/go-shiori/go-readability v0.0.0-20210627123243-82cc33435520
@@ -26,7 +27,9 @@ require (
 	code.rocketnine.space/tslocum/cbind v0.1.5 // indirect
 	github.com/alecthomas/chroma v0.8.2 // indirect
 	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -40,20 +43,23 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-runewidth v0.0.14-0.20210830053702-dc8fe66265af // indirect
 	github.com/microcosm-cc/bluemonday v1.0.16 // indirect
-	github.com/muesli/reflow v0.2.1-0.20210115123740-9e1d0d53df68 // indirect
-	github.com/muesli/termenv v0.9.0 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.0 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tkuchiki/go-timezone v0.2.0 // indirect
 	github.com/yuin/goldmark v1.3.5 // indirect
 	github.com/yuin/goldmark-emoji v1.0.1 // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
-	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
-	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
