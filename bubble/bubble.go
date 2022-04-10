80bf34c6d6bdd63a5b922f720ea66a328dd07f19
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-04-10 09:57:31 +0200

    Remove spacing and margins

--- bubble/bubble.go
+++ bubble/bubble.go
@@ -2,12 +2,12 @@ package bubble
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"os"
+	"os/exec"
 )
 
 var docStyle = lipgloss.NewStyle().Margin(0, 0)
@@ -46,6 +46,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 		if msg.String() == "enter" {
 			return m, openEditor()
+		}
+		if msg.String() == "e" {
+			cmd := m.list.StartSpinner()
+			return m, cmd
 		}
 
 	case tea.WindowSizeMsg:
@@ -87,21 +91,75 @@ func Run() {
 		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
 		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
 		item{title: "Terrycloth", desc: "In other words, towel fabric"},
+		item{title: "Raspberry Pi’s", desc: "I have ’em all over my house"},
+		item{title: "Nutella", desc: "It's good on toast"},
+		item{title: "Bitter melon", desc: "It cools you down"},
+		item{title: "Nice socks", desc: "And by that I mean socks without holes"},
+		item{title: "Eight hours of sleep", desc: "I had this once"},
+		item{title: "Cats", desc: "Usually"},
+		item{title: "Plantasia, the album", desc: "My plants love it too"},
+		item{title: "Pour over coffee", desc: "It takes forever to make though"},
+		item{title: "VR", desc: "Virtual reality...what is there to say?"},
+		item{title: "Noguchi Lamps", desc: "Such pleasing organic forms"},
+		item{title: "Linux", desc: "Pretty much the best OS"},
+		item{title: "Business school", desc: "Just kidding"},
+		item{title: "Pottery", desc: "Wet clay is a great feeling"},
+		item{title: "Shampoo", desc: "Nothing like clean hair"},
+		item{title: "Table tennis", desc: "It’s surprisingly exhausting"},
+		item{title: "Milk crates", desc: "Great for packing in your extra stuff"},
+		item{title: "Afternoon tea", desc: "Especially the tea sandwich part"},
+		item{title: "Stickers", desc: "The thicker the vinyl the better"},
+		item{title: "20° Weather", desc: "Celsius, not Fahrenheit"},
+		item{title: "Warm light", desc: "Like around 2700 Kelvin"},
+		item{title: "The vernal equinox", desc: "The autumnal equinox is pretty good too"},
+		item{title: "Gaffer’s tape", desc: "Basically sticky fabric"},
+		item{title: "Terrycloth", desc: "In other words, towel fabric"},
 	}
 
 	// Create a new default delegate
 	d := list.NewDefaultDelegate()
+	d.SetSpacing(0)
 
 	// Change colors
 	c := lipgloss.Color("#6f03fc")
-	d.Styles.SelectedTitle = d.Styles.SelectedTitle.Foreground(c).BorderLeftForeground(c)
-	d.Styles.SelectedDesc = d.Styles.SelectedTitle.Copy() // reuse the title style here
+	d.Styles.SelectedTitle = d.Styles.SelectedTitle.
+		Foreground(c).
+		UnsetPadding().
+		UnsetBorderStyle().
+		UnsetMargins()
+	d.Styles.NormalTitle = d.Styles.NormalTitle.
+		UnsetBorderStyle().
+		UnsetPadding().
+		UnsetMargins()
+	d.Styles.NormalDesc = d.Styles.NormalTitle.Copy() // reuse the title style here
+
+	d.Styles.SelectedTitle = d.Styles.SelectedTitle.
+		UnsetBorderStyle().
+		UnsetPadding()
+
+	d.Styles.DimmedTitle = d.Styles.DimmedTitle.
+		UnsetBorderStyle().
+		UnsetPadding()
+	//d.Styles.SelectedTitle = d.Styles.SelectedTitle.UnsetPadding()
+	d.Styles.SelectedTitle = d.Styles.SelectedTitle.BorderLeft(false)
+	d.Styles.SelectedDesc = d.Styles.SelectedTitle.Copy()           // reuse the title style here
+	d.Styles.SelectedDesc = d.Styles.SelectedDesc.BorderLeft(false) // reuse the title style here
+
+	d.Styles.NormalTitle = d.Styles.NormalTitle.BorderLeft(false)
+	d.Styles.NormalDesc = d.Styles.NormalDesc.BorderLeft(false)
 
 	m := model{list: list.New(items, d, 0, 0)}
 
-	m.list.Title = "My Fave Things"
+	m.list.SetShowTitle(false)
+	m.list.SetShowPagination(false)
+	m.list.SetShowHelp(false)
 	m.list.SetFilteringEnabled(false)
 	m.list.SetShowStatusBar(false)
+	m.list.Styles.Title = m.list.Styles.Title.UnsetBorderStyle().
+		UnsetPadding().
+		UnsetMargins()
+
+	m.list.SetSpinner(spinner.Globe)
 
 	p := tea.NewProgram(m, tea.WithAltScreen())
 
