c459014a183b36076e94fced98edb835af4dfa00
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2022-05-17 07:54:45 +0200

    Implement non persistent functions

--- history/non-persistent.go
+++ history/non-persistent.go
@@ -1,28 +1,21 @@
 package history
 
+import "time"
+
 type NonPersistent struct{}
 
-func (NonPersistent) Contains(id int) bool {
-	//TODO implement me
-	panic("implement me")
+func (NonPersistent) Contains(_ int) bool {
+	return false
 }
 
-func (NonPersistent) GetLastVisited(id int) int64 {
-	//TODO implement me
-	panic("implement me")
+func (NonPersistent) GetLastVisited(_ int) int64 {
+	return time.Now().Unix()
 }
 
-func (NonPersistent) GetLastCommentCount(id int) int {
-	//TODO implement me
-	panic("implement me")
+func (NonPersistent) GetLastCommentCount(_ int) int {
+	return 0
 }
 
-func (NonPersistent) ClearAndWriteToDisk() {
-	//TODO implement me
-	panic("implement me")
-}
+func (NonPersistent) ClearAndWriteToDisk() {}
 
-func (NonPersistent) AddToHistoryAndWriteToDisk(id int, commentsOnLastVisit int) {
-	//TODO implement me
-	panic("implement me")
-}
+func (NonPersistent) AddToHistoryAndWriteToDisk(_ int, _ int) {}
