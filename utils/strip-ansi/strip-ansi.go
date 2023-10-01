6f9de56226fef59e08f77b7dd247b7a5805aa7e0
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-10-01 14:27:29 +0200

    Format

--- utils/strip-ansi/strip-ansi.go
+++ utils/strip-ansi/strip-ansi.go
@@ -4,10 +4,12 @@ import (
 	"regexp"
 )
 
-const esc = "[\u001B\u009B]"
-const oscSequence = "(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007"
-const csiSequence = "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~])"
-const ansi = esc + "[[\\]()#;?]*" + "(?:" + oscSequence + "|" + csiSequence + ")"
+const (
+	esc         = "[\u001B\u009B]"
+	oscSequence = "(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007"
+	csiSequence = "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~])"
+	ansi        = esc + "[[\\]()#;?]*" + "(?:" + oscSequence + "|" + csiSequence + ")"
+)
 
 var ansiRegex = regexp.MustCompile(ansi)
 
