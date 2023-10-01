fa8baaf91eaec1b4c6bf04c028695d2dff650d0c
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-10-01 14:43:13 +0200

    Adjust regexp to include both CSI and OSC sequences

--- utils/strip-ansi/strip-ansi.go
+++ utils/strip-ansi/strip-ansi.go
@@ -4,7 +4,10 @@ import (
 	"regexp"
 )
 
-var combinedRegex = regexp.MustCompile(`(\x1B\[|\x9B\[|\\u001b\[|\\u009b\[)[0-?]*[ -/]*[@-~]`)
+var combinedRegex = regexp.MustCompile(
+	`(\x1B\[|\x9B\[|\\u001b\[|\\u009b\[)[0-?]*[ -/]*[@-~]|` + // CSI Sequences
+		`\x1B\]|\x9B].*?(\007|\x1B\\)|\\u001b\]|\\u009b\].*?(\\u0007|\\u001b\\)`, // OSC Sequences
+)
 
 func Strip(text string) string {
 	return combinedRegex.ReplaceAllString(text, "")
